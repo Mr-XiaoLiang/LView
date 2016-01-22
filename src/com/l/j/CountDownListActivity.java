6924db98a9c3df48d4a8bab118c222bc01ec0fcf
LiuJ <liujian594@hotmail.com>
2016-01-22 17:58:15 +0800

    2016-01-22

---
+++ src/com/l/j/CountDownListActivity.java
@@ -0,0 +1,230 @@
+package com.l.j;
+
+import java.util.ArrayList;
+
+import com.l.j.view.LCountDownView;
+import com.l.j.view.LCountDownView.LCountDownViewListener;
+import com.l.j.view.LGoodsSizeChangeView;
+import com.l.j.view.LGoodsSizeChangeView.OnLGoodsSizeChangeViewListener;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * 倒计时View测试类
+ * 
+ * @author LiuJ
+ *
+ */
+public class CountDownListActivity extends Activity {
+	/**
+	 * 数据集
+	 */
+	private ArrayList<Bean> beans;
+
+	private ListView list;
+
+	private TextView hint;
+
+	private ShowAdapter adapter;
+	
+	private CDListener listener;
+	
+	private GSCListener listener2;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.count_down);
+		list = (ListView) findViewById(R.id.count_down_list);
+		hint = (TextView) findViewById(R.id.count_down_hint);
+		hint.setText("每个item的时间在初始化的时候是相差1分钟");
+
+		init();
+
+	}
+
+	private void init() {
+		beans = new ArrayList<CountDownListActivity.Bean>();
+		for (int i = 0; i < 50; i++) {
+			CountDownListActivity.Bean bean = new Bean(System.currentTimeMillis(), "这是第"+(i+1)+"条，起始：" + i + ":00", (i*60),i+1);
+			beans.add(bean);
+		}
+		listener = new CDListener();
+		listener2 = new GSCListener();
+		list.setAdapter(adapter = new ShowAdapter());
+		list.invalidate();
+		adapter.notifyDataSetInvalidated();
+	}
+
+	private class ShowAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			return beans.size();
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return beans.get(position);
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return position;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			Holder holder = null;
+			if (convertView == null) {
+				LayoutInflater layoutInflator = LayoutInflater.from(parent.getContext());
+				convertView = layoutInflator.inflate(R.layout.count_down_item, null);
+				holder = new Holder();
+				holder.text = (TextView) convertView.findViewById(R.id.count_down_item_text);
+				holder.downView = (LCountDownView) convertView.findViewById(R.id.count_down_item_view);
+				holder.changeView = (LGoodsSizeChangeView) convertView.findViewById(R.id.count_down_item_size);
+				convertView.setTag(holder);
+			} else {
+				holder = (Holder) convertView.getTag();
+			}
+			holder.text.setText(beans.get(position).getText());
+			holder.downView.setTime(position, 0, beans.get(position).getData(), beans.get(position).getTimestamp());
+			holder.downView.setListener(listener);
+			holder.changeView.setGoodsSize(position,beans.get(position).getSize());
+			holder.changeView.setListener(listener2);
+			return convertView;
+		}
+
+		class Holder {
+			TextView text;
+			LCountDownView downView;
+			LGoodsSizeChangeView changeView;
+		}
+	}
+
+	private class GSCListener implements OnLGoodsSizeChangeViewListener{
+		@Override
+		public void onGoodSizeChange(int thisPosition, String thisID, int goodsSize) {
+			Bundle bundle = new Bundle();
+			bundle.putInt("thisPosition", thisPosition);
+			bundle.putInt("goodsSize", goodsSize);
+			Message message = new Message();
+			message.what = 3;
+			message.setData(bundle);
+			handler.sendMessage(message);
+		}
+	}
+	
+	private class CDListener implements LCountDownViewListener {
+		@Override
+		public void onCountDown(int thisPosition, int thisID, int second, String date, long timestamp) {
+			Bundle bundle = new Bundle();
+			bundle.putInt("thisPosition", thisPosition);
+			bundle.putInt("date", second);
+			bundle.putLong("timestamp", timestamp);
+			Message message = new Message();
+			message.what = 1;
+			message.setData(bundle);
+			handler.sendMessage(message);
+		}
+
+		@Override
+		public void onCountDownForEnd(int thisPosition, int thisID) {
+			Bundle bundle = new Bundle();
+			bundle.putInt("thisPosition", thisPosition);
+			Message message = new Message();
+			message.what = 2;
+			message.setData(bundle);
+			handler.sendMessage(message);
+		}
+	}
+
+	Handler handler = new Handler() {
+
+		@Override
+		public void handleMessage(Message msg) {
+			Bundle bundle;
+			switch (msg.what) {
+			case 1:
+				bundle = msg.getData();
+				beans.get(bundle.getInt("thisPosition")).setTime(bundle.getInt("date"), bundle.getLong("timestamp"));
+				break;
+			case 2:
+				bundle = msg.getData();
+				beans.get(bundle.getInt("thisPosition")).setText("倒计时结束");
+				break;
+			case 3:
+				bundle = msg.getData();
+				beans.get(bundle.getInt("thisPosition")).setSize(bundle.getInt("goodsSize"));
+				break;
+			default:
+				break;
+			}
+			super.handleMessage(msg);
+		}
+
+	};
+
+	private class Bean {
+		private Long timestamp;
+		private String text;
+		private int data;
+		private int size;
+
+		public Bean(Long timestamp, String text, int data,int Size) {
+			super();
+			this.timestamp = timestamp;
+			this.text = text;
+			this.data = data;
+			this.size = Size;
+		}
+
+		public Long getTimestamp() {
+			return timestamp;
+		}
+
+		public void setTimestamp(Long timestamp) {
+			this.timestamp = timestamp;
+		}
+
+		public String getText() {
+			return text;
+		}
+
+		public void setText(String text) {
+			this.text = text;
+		}
+
+		public int getData() {
+			return data;
+		}
+
+		public void setData(int data) {
+			this.data = data;
+		}
+
+		public void setTime(int data, Long timestamp) {
+			this.data = data;
+			this.timestamp = timestamp;
+		}
+
+		public int getSize() {
+			return size;
+		}
+
+		public void setSize(int size) {
+			this.size = size;
+		}
+		
+	}
+
+}
--- src/com/l/j/MainActivity.java
+++ src/com/l/j/MainActivity.java
@@ -29,10 +29,12 @@ public class MainActivity extends Activity implements OnItemClickListener {
 
 	private ListView listView;
 
-	private String[] names = { "日历选择-日期没有限制&全向拖动", "日历选择-日期有限制&单向", "选项卡切换-三角形",
-			"选项卡切换-线形", "时间选择-小时", "时间选择-分钟",
-			"加载等待动画", "饼图", "进度图", "雷达图",
-			"圆形图片", "滑动按钮", "温度计","进度条按钮","页面下方小点","tab小点","日期滚轮","时间滚轮","全套滚轮","tab条形"};
+	private String[] names = { 
+			"日历选择-日期没有限制&全向拖动", "日历选择-日期有限制&单向", "选项卡切换-三角形", "选项卡切换-线形", "时间选择-小时", 
+			"时间选择-分钟","加载等待动画", "饼图", "进度图", "雷达图", 
+			"圆形图片", "滑动按钮", "温度计", "进度条按钮", "页面下方小点", 
+			"tab小点", "日期滚轮", "时间滚轮", "全套滚轮","tab条形", 
+			"倒计时View" };
 
 	private DialogUtil dialogUtil;
 	private Calendar calendar;
@@ -43,7 +45,8 @@ public class MainActivity extends Activity implements OnItemClickListener {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		listView = (ListView) findViewById(R.id.main_list);
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.dialog_list_item, R.id.dialog_list_item_name, names);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.dialog_list_item,
+				R.id.dialog_list_item_name, names);
 		listView.setAdapter(adapter);
 		adapter.notifyDataSetChanged();
 		listView.invalidate();
@@ -51,95 +54,89 @@ public class MainActivity extends Activity implements OnItemClickListener {
 		dialogUtil = new DialogUtil();
 		calendar = Calendar.getInstance();
 	}
-	
+
 	@Override
 	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 		switch (arg2) {
-			case 0:
-				dialogUtil.getCalendarDialog(this,
-						calendar.get(Calendar.MONTH) + 1,
-						calendar.get(Calendar.YEAR),
-						calendar.get(Calendar.DAY_OF_MONTH),LCalendarView.SlideType.Both,
-						new CalendarDialogListener() {
-							@Override
-							public void calendarDialogListener(int year, int month,
-															   int day) {
-								t(year + "-" + month + "-" + day);
-							}
-						});
-				break;
-			case 1:
-				dialogUtil.getCalendarDialog(this,
-						calendar.get(Calendar.MONTH) + 1,
-						calendar.get(Calendar.YEAR),
-						calendar.get(Calendar.DAY_OF_MONTH),
-						calendar.get(Calendar.MONTH)+1,
-						calendar.get(Calendar.YEAR),
-						calendar.get(Calendar.DAY_OF_MONTH),
-						LCalendarView.SlideType.Horizontal,
-						new CalendarDialogListener() {
-							@Override
-							public void calendarDialogListener(int year, int month,
-															   int day) {
-								t(year + "-" + month + "-" + day);
-							}
-						});
-				break;
-			case 2:
-				intent = new Intent(this, ChooseBgTest.class);
-				intent.putExtra("type", true);
-				startActivity(intent);
-				break;
-			case 3:
-				intent = new Intent(this, ChooseBgTest.class);
-				intent.putExtra("type", false);
-				startActivity(intent);
-				break;
-			case 4:
-				intent = new Intent(this, ClockTest.class);
-				intent.putExtra("type", LClockView.TYPE_HOURS);
-				startActivity(intent);
-				break;
-			case 5:
-				intent = new Intent(this, ClockTest.class);
-				intent.putExtra("type", LClockView.TYPE_MINUTES);
-				startActivity(intent);
-				break;
-			case 6:
-				dialogUtil.getLoadDialog(this);
-				break;
-			case 8:
-				dialogUtil.getProgressDialog(this, 100, 100);
-				break;
-			case 14:
-				intent = new Intent(this, PageTest.class);
-				intent.putExtra("type", PageTest.thisType_bottom);
-				startActivity(intent);
-				break;
-			case 15:
-				intent = new Intent(this, PageTest.class);
-				intent.putExtra("type", PageTest.thisType_top);
-				startActivity(intent);
-				break;
-			case 16:
-				dialogUtil.getWheelDialog(this, LWheelDialogType.DATE, null);
+		case 0:
+			dialogUtil.getCalendarDialog(this, calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR),
+					calendar.get(Calendar.DAY_OF_MONTH), LCalendarView.SlideType.Both, new CalendarDialogListener() {
+						@Override
+						public void calendarDialogListener(int year, int month, int day) {
+							t(year + "-" + month + "-" + day);
+						}
+					});
 			break;
-			case 17:
-				dialogUtil.getWheelDialog(this, LWheelDialogType.TIME, null);
-				break;
-			case 18:
-				dialogUtil.getWheelDialog(this, LWheelDialogType.ALL, null);
-				break;
-			case 19:
-				intent = new Intent(this, PageTest.class);
-				intent.putExtra("type", PageTest.thisType_top_line);
-				startActivity(intent);
-				break;
-			default:
-				intent = new Intent(this, ViewTest.class);
-				intent.putExtra("type", arg2-7);
-				startActivity(intent);
-				break;
+		case 1:
+			dialogUtil.getCalendarDialog(this, calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR),
+					calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH) + 1, calendar.get(Calendar.YEAR),
+					calendar.get(Calendar.DAY_OF_MONTH), LCalendarView.SlideType.Horizontal,
+					new CalendarDialogListener() {
+						@Override
+						public void calendarDialogListener(int year, int month, int day) {
+							t(year + "-" + month + "-" + day);
+						}
+					});
+			break;
+		case 2:
+			intent = new Intent(this, ChooseBgTest.class);
+			intent.putExtra("type", true);
+			startActivity(intent);
+			break;
+		case 3:
+			intent = new Intent(this, ChooseBgTest.class);
+			intent.putExtra("type", false);
+			startActivity(intent);
+			break;
+		case 4:
+			intent = new Intent(this, ClockTest.class);
+			intent.putExtra("type", LClockView.TYPE_HOURS);
+			startActivity(intent);
+			break;
+		case 5:
+			intent = new Intent(this, ClockTest.class);
+			intent.putExtra("type", LClockView.TYPE_MINUTES);
+			startActivity(intent);
+			break;
+		case 6:
+			dialogUtil.getLoadDialog(this);
+			break;
+		case 8:
+			dialogUtil.getProgressDialog(this, 100, 100);
+			break;
+		case 14:
+			intent = new Intent(this, PageTest.class);
+			intent.putExtra("type", PageTest.thisType_bottom);
+			startActivity(intent);
+			break;
+		case 15:
+			intent = new Intent(this, PageTest.class);
+			intent.putExtra("type", PageTest.thisType_top);
+			startActivity(intent);
+			break;
+		case 16:
+			dialogUtil.getWheelDialog(this, LWheelDialogType.DATE, null);
+			break;
+		case 17:
+			dialogUtil.getWheelDialog(this, LWheelDialogType.TIME, null);
+			break;
+		case 18:
+			dialogUtil.getWheelDialog(this, LWheelDialogType.ALL, null);
+			break;
+		case 19:
+			intent = new Intent(this, PageTest.class);
+			intent.putExtra("type", PageTest.thisType_top_line);
+			startActivity(intent);
+			break;
+		case 20:
+			intent = new Intent(this, CountDownListActivity.class);
+			startActivity(intent);
+			break;
+		default:
+			intent = new Intent(this, ViewTest.class);
+			intent.putExtra("type", arg2 - 7);
+			startActivity(intent);
+			break;
 		}
 	}
 
